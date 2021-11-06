77dc409ef08354550fd116eb7f5610a21deafe5c
Vini <viniciosgois17@gmail.com>
2021-11-06 18:50:07 -0300

    challenge finalized

--- src/index.js
+++ src/index.js
@@ -10,20 +10,67 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
-}
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found' });
+  };
+
+  request.user = user;
+  return next();
+};
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (user.todos.length < 10 || user.pro === true) {
+    return next();
+  } else {
+    return response.status(403).json({ error: "Upgrade to pro and unlock all features" });
+  }
+  
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
-}
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const userExists = users.find(user => user.username === username);
+
+  if (!userExists) {
+    return response.status(404).json({ error: 'User not found' });
+  };
+
+  const validator = validate(id);
+  if(!validator) {
+    return response.status(400).json({ error: 'Validator do not worked' });
+  };
+  
+  const todoExists = userExists.todos.find(todo => todo.id === id);
+  
+  if (!todoExists) {
+    return response.status(404).json({ error: 'Todo not found' });
+  };
+  
+  request.user = userExists;
+  request.todo = todoExists;
+  return next();
+};
 
 function findUserById(request, response, next) {
-  // Complete aqui
-}
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+  
+  if (!user) {
+    return response.status(404).json({ error: 'User not found' });
+  }
+  request.user = user;
+  return next();
+};
 
 app.post('/users', (request, response) => {
   const { name, username } = request.body;
